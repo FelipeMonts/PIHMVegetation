50f1dee76470cb0da035707265a93c1e5d06b497
FelipeMonts <frm10@psu.edu>
2017-10-17 19:31:52 -0400

    Created a translation table from NLCD to PIHM landcover data

--- LandCover.R
+++ LandCover.R
@@ -16,7 +16,7 @@
 #  Set Working directory
 
 
-setwd("C:/Felipe/PIHM-CYCLES/PIHM/PIHM_Felipe/CNS/WE-38/WE38_Files_PIHM_Cycles20170208/SWATPIHMRcode") ; 
+setwd("C:/Felipe/PIHM-CYCLES/PIHM/PIHM_R_Scripts/PIHMVegetation") ; 
 
 ########### Install packages  #####################
 
@@ -27,4 +27,35 @@ setwd("C:/Felipe/PIHM-CYCLES/PIHM/PIHM_Felipe/CNS/WE-38/WE38_Files_PIHM_Cycles20
 ########### Call the library packages needed for the program to work #############
 
 # load libraries
-library(Hmisc) ;
+#library(Hmisc) ;
+
+############# Load the vegetation parameter table and the convertion parameters for PIHM - MM ################
+
+NUMLC<-read.table("./vegprmt.tbl", skip=0, as.is=T, nrows=1) ;
+
+
+vegprmt.tbl<-read.table("./vegprmt.tbl", skip=1, sep="", as.is=T, header=T, nrows=NUMLC[1,2]) ;
+
+Description<-read.table("./vegprmt.tbl", skip=1, sep="\t", as.is=T, header=T, nrows=NUMLC[1,2], comment.char="") ;
+
+vegprmt.tbl$Description<-sapply(strsplit(Description[,1], "#"), "[" , 2) ;
+
+Otherprmt.tbl<-read.table("./vegprmt.tbl", skip=NUMLC[1,2]+2, sep="", as.is=T, header=F, nrows=5) ;
+
+
+
+############# Load the vegetation parameter map from the NLCD to the MM-PIHM Land Cover type 
+############# "NLCD land cover class mapping to PIHM land cover type ############################          
+
+
+NLCD_PIHM.lc<-read.table("./vegprmt.tbl", skip=NUMLC[1,2]+10, sep= ">" , as.is=T, header=F,comment.char="") ;
+
+PIHM.lc<-NLCD_PIHM.lc[,2];
+
+NLCD.lc<-as.integer(sapply(strsplit(NLCD_PIHM.lc[,1], split = " "), "[" , 2)) ;
+
+NLCD_to_PIHM<-merge(data.frame(NLCD.lc, PIHM.lc), vegprmt.tbl, by.x= "PIHM.lc", by.y= "INDEX", all=T) ;
+
+
+
+
